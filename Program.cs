2a8e2ed9e96afd7b5ad034f2390cfa5a44b021b7
AlfaProgrammer <alexloghin@outlook.it>
2022-06-23 17:06:29 +0200

    Finito Stampa preferiti con input utente

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibiPreferiti = { "Pizza", "Pasta al ragù", "Petto di pollo", "Costine", "Riso" };
+string[] cibiPreferiti = { "Pizza", "Pasta al ragù", "Petto di pollo", "Costine", "Riso", "Pasta di riso", "Sushi", "Pasta di soia", "Pesce" };
 
 int arrLength = cibiPreferiti.Length;
 
@@ -22,4 +22,47 @@ Console.WriteLine("Cibo di mezza classifica");
 for (int i = 1; i < cibiPreferiti.Length - 1 ; i++)
 {
     Console.WriteLine ($"Index: {i}, cibo: {cibiPreferiti[i]}");
+}
+
+string userInput;
+
+do
+{
+    Console.Write("Quale è il tuo cibo preferito?: ");
+
+    userInput = Console.ReadLine();
+
+} while (userInput == "");
+
+do {
+    Console.WriteLine("Questo cibo non è presente, Insrisci nuovamente: ");
+    userInput = Console.ReadLine();
+
+} while (!cibiPreferiti.Contains(userInput));
+
+Random rand = new Random();
+int computerNumber = rand.Next(0, cibiPreferiti.Length - 1);
+
+Console.WriteLine($"Stampa Array con le rispettive scelte"); 
+
+loopIndex = 0;
+
+foreach (string item in cibiPreferiti)
+{
+    if (item == cibiPreferiti[computerNumber] && item == userInput)
+    {
+        Console.WriteLine($"{item}: -------> Abbiamo gli stessi gusti");
+    }    
+    else if (item == cibiPreferiti[computerNumber])
+    {
+        Console.WriteLine($"{item} ------> Scelta Computer");
+    }
+    else if (item == userInput)
+    {
+        Console.WriteLine($"{item} ------> Scelta Utente");
+    }    
+    else
+    {
+        Console.WriteLine(item);
+    }    
 }
